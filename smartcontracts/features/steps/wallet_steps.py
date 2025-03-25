c9697e7d85ffe2ba3e27d0795ba4cdab109f8915
Lucas Oliveira <olivmath@protonmail.com>
2025-03-25 07:49:50 -0300

    update: wallet steps

--- smartcontracts/features/steps/wallet_steps.py
+++ smartcontracts/features/steps/wallet_steps.py
@@ -1,15 +1,21 @@
+from behave import given
+
 from stellar_sdk import Keypair
 import requests
 
-def create_and_fund_wallet(context, wallet_name):
-    """Create and fund a wallet with a given name."""
-    keypair = Keypair.random()
-    setattr(context, f"{wallet_name}_keypair", keypair)
+
+@given('I create a wallet from "{wallet_priv}" called "{wallet_name}" and funded')
+def step_impl(context, wallet_priv, wallet_name):
+    """Create a wallet from a given seed and assign it a name."""
+    keypair = Keypair.from_secret(wallet_priv)
+
+    context.wallets[wallet_name] = {
+        "prv": keypair.public_key,
+        "pub": keypair.secret,
+        "keypair": keypair,
+    }
+
     try:
         requests.get(f"{context.stellar_url}/friendbot?addr={keypair.public_key}")
     except Exception as e:
         raise AssertionError(f"Could not fund {wallet_name} wallet: {str(e)}")
-
-@given('I have a "{wallet_name}" wallet funded')
-def step_setup_wallet(context, wallet_name):
-    create_and_fund_wallet(context, wallet_name)
